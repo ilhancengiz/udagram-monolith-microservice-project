aca320882946d014538dce27541e01b848b076ad
İlhan Cengiz <ilhancengiz@live.com>
2021-02-03 23:29:37 +0300

    [feat] : /health endpoints added to apis for checking db connection availability. [fix] : fixes made in deployment files for env variable keys.

--- udagram-api/user-api/src/server.ts
+++ udagram-api/user-api/src/server.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import { sequelize } from "./sequelize";
+const { v4: uuidv4 } = require("uuid");
 
 import { IndexRouter } from "./controllers/v0/index.router";
 
@@ -40,6 +41,30 @@ import { requestLogger } from "./util/requestLogger";
   // Root URI call
   app.get("/", async (req, res) => {
     res.send("/api/v0/");
+  });
+
+  app.get("/health", async (req, res) => {
+    let pid = uuidv4();
+    console.log(
+      new Date().toLocaleString() + `: ${pid} - Checking database connection...`
+    );
+    try {
+      await sequelize.authenticate();
+      console.log(
+        new Date().toLocaleString() +
+          `: ${pid} - Database Connection has been established successfully`
+      );
+      return res.status(200).send({
+        message:
+          new Date().toLocaleString() +
+          `: ${pid} - Connection has been established successfully.`,
+      });
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+      return res
+        .status(400)
+        .send({ message: `Unable to connect to the database: ${error}` });
+    }
   });
 
   // Start the Server
